6deee0a98bc52052e5a630ee263135bed1bddeea
Leftyxiv <manny@aledoux.net>
2020-12-30 12:11:59 -0500

    added user role restrictions to route middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -90,3 +90,16 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    //ROLES IS ARRAY ['ADMIN', 'LEAD-GUIDE']. ROLE = 'USER'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action'),
+        403
+      );
+    }
+    next();
+  };
+};
