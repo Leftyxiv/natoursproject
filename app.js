6c3aa15c2800248cca9ff39978088d152efb326b
Leftyxiv <manny@aledoux.net>
2020-12-31 13:19:02 -0500

    completed user set up and authentication

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 //ERROR HANDLER IMPORTS
 const AppError = require('./utils/appError');
@@ -12,17 +17,53 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //MIDDLEWARE
+//DEV LOGGING
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+//SECURITY HHTP HEADERS
+app.use(helmet());
 
-app.use(express.json());
+//RATELIMIT REQUESTS FROM SAME IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
+//READING DATA FROM THE BODY
+app.use(express.json({ limit: '10kb' }));
+
+//DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+//DATA SANITIZATION AGAINST XSS
+app.use(xss());
+
+//PARAMETER POLLUTION PREVENTION
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+//SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
 //   console.log('hello from the middleware');
 //   next();
 // });
+
+//TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   //console.log(req.headers);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,15 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 //12.7.0.01/api/v1/users/
 router
   .route('/')
