16a5260ac9670bcb24075b3dfc80a958f3d4842c
Leftyxiv <manny@aledoux.net>
2021-01-12 10:32:08 -0500

    finished overview page

---
+++ controllers/viewsController.js
@@ -0,0 +1,20 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //get tour data from collection
+  const tours = await Tour.find();
+
+  //build template
+  //render that template using the tour data from step one
+  res.status(200).render('main', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+};
