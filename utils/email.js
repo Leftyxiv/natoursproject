12def4c0c181a33854b0bd6b0be5ea30d39c5b98
Leftyxiv <manny@aledoux.net>
2021-01-22 12:22:39 -0500

    crearted new email class to send emails with

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,56 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  //CREATE A TRANSPORTER
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
 
-  //DEFINE THE EMAIL OPTIONS
-  const mailOptions = {
-    from: 'Manny <Manny@aledoux.net>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    //html
-  };
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Manny <${process.env.EMAIL_FROM}>`;
+  }
 
-  // ACTUALLY SEND THE EMAIL
-  await transporter.sendMail(mailOptions);
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //sendgrid
+      return 0;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    //send the actual email
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+      //html
+    };
+
+    // await transporter.sendMail(mailOptions);
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
 };
-
-module.exports = sendEmail;
